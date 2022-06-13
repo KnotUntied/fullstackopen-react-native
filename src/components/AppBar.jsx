ea2f3b1a18ad204d83ce886150f620bf0df6bb93
Andrei Tan <20182921+KnotUntied@users.noreply.github.com>
2022-06-13 13:59:35 +0800

    10.6

--- src/components/AppBar.jsx
+++ src/components/AppBar.jsx
@@ -1,6 +1,7 @@
 import { View, StyleSheet } from 'react-native';
+import { Link } from "react-router-native";
+import Text from './Text';
 import Constants from 'expo-constants';
-import AppBarTab from './AppBarTab';
 
 import theme from '../theme';
 
@@ -10,15 +11,31 @@ const styles = StyleSheet.create({
     color: 'white',
     padding: 24,
     paddingTop: 24 + Constants.statusBarHeight,
-    // ...
+    flexDirection: 'row',
   },
-  // ...
+  tab: {
+    color: 'white',
+    marginRight: 24,
+  },
 });
 
+const AppBarTab = ({ to, children }) => {
+  return (
+    <View>
+      <Link to={to}>
+        <Text fontWeight="bold" fontSize="subheading" style={styles.tab}>{children}</Text>
+      </Link>
+    </View>
+  );
+};
+
 const AppBar = () => {
-  return <View style={styles.container}>
-    <AppBarTab>Repositories</AppBarTab>
-  </View>;
+  return (
+    <View style={styles.container}>
+      <AppBarTab to='/'>Repositories</AppBarTab>
+      <AppBarTab to='/login'>Sign In</AppBarTab>
+    </View>
+  );
 };
 
 export default AppBar;
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,11 +1,14 @@
-import Constants from 'expo-constants';
-import { Text, StyleSheet, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+import { Route, Routes, Navigate } from 'react-router-native';
+
 import AppBar from './AppBar';
 import RepositoryList from './RepositoryList';
+import SignIn from './SignIn';
+import theme from '../theme';
 
 const styles = StyleSheet.create({
   container: {
-    backgroundColor: '#e1e4e8',
+    backgroundColor: theme.colors.mainBackground,
     flexGrow: 1,
     flexShrink: 1,
   },
@@ -15,7 +18,11 @@ const Main = () => {
   return (
     <View style={styles.container}>
       <AppBar />
-      <RepositoryList />
+      <Routes>
+        <Route path="/" element={<RepositoryList />} exact />
+        <Route path="/login" element={<SignIn />} exact />
+        <Route path="*" element={<Navigate to="/" replace />} />
+      </Routes>
     </View>
   );
 };
